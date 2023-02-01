6416b0675b03bc40f0adc88bbb232892522e6c61
Robert Rozehnal <rob.kab.roz@gmail.com>
2023-02-01 23:20:08 +0100

    finally working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MainContainer from "./components/Main/MainContainer";
+import NavMobile from "./components/Main/NavMobile";
 import AlertDialog from "./components/UI/AlertDialog";
 import SnackbarWindow from "./components/UI/SnackbarWindow";
 
 const App = (): JSX.Element => {
   return (
     <>
+      <NavMobile />
       <MainContainer />
       <SnackbarWindow />
       <AlertDialog />
